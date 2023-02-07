b0222963dfff87d8c69185282f006453eca63bba
Studawatta <st.udawatte@gmail.com>
2023-02-07 22:20:14 +0530

    filter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
 import pokemon from './pokemon.json';
@@ -20,6 +20,7 @@ PokemonRow.prototype = {
 };
 
 function App() {
+  const [filter, filterSet] = useState('');
   return (
     <div
       style={{
@@ -29,23 +30,41 @@ function App() {
       }}
     >
       <h1 className="title">Pokemon Search</h1>
-      <input />
-      <table width="100%">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {pokemon.slice(0, 20).map((pokemon) => (
-            <PokemonRow
-              pokemon={pokemon}
-              key={pokemon.id}
-            />
-          ))}
-        </tbody>
-      </table>
+      <input
+        value={filter}
+        onChange={(e) => filterSet(e.target.value)}
+      />
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '70% 30%',
+          gridColumnGap: '1rem',
+        }}
+      >
+        <table width="100%">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pokemon
+              .filter((pokemon) =>
+                pokemon.name.english
+                  .toLowerCase()
+                  .includes(filter.toLowerCase())
+              )
+              .slice(0, 20)
+              .map((pokemon) => (
+                <PokemonRow
+                  pokemon={pokemon}
+                  key={pokemon.id}
+                />
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
